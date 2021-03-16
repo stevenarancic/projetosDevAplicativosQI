26ad1d5e8da497e14557a51ea07a5e9603417720
stevenarancic <58119793+stevenarancic@users.noreply.github.com>
2021-03-16 16:32:01 -0300

    Add files via upload

---
+++ Desafio1603/projetoDesafio/src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner ler = new Scanner(System.in);
+
+        double[] valores = new double[5];
+
+        System.out.print("Digite um valor: ");
+        valores[0] = ler.nextDouble();
+
+        System.out.print("Digite mais um valor: ");
+        valores[1] = ler.nextDouble();
+
+        System.out.print("Digite mais um valor: ");
+        valores[2] = ler.nextDouble();
+
+        System.out.print("Digite mais um valor: ");
+        valores[3] = ler.nextDouble();
+
+        System.out.print("Digite mais um valor: ");
+        valores[4] = ler.nextDouble();
+
+        System.out.print(valores[0] + ", " + valores[1] + ", " + valores [2] + ", " + valores[3] + ", " + valores[4]);
+    }
+}
